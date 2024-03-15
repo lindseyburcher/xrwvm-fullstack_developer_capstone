534c028c1dbe0e6779a066d9cb95692f1378c7a6
lindseyburcher <lindseyburcher@gmail.com>
2024-03-15 07:10:46 -0400

    linting

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -14,6 +14,7 @@ sentiment_analyzer_url = os.getenv(
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
 
+
 def get_request(endpoint, **kwargs):
     params = ""
     if (kwargs):
