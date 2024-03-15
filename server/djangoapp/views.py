0a388eb2b901f2e15fd53f9fd782e29cc398b5c2
lindseyburcher <lindseyburcher@gmail.com>
2024-03-15 16:46:10 -0400

    linter code

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -127,10 +127,12 @@ def add_review(request):
         data = json.loads(request.body)
         try:
             response = post_review(data)
+            print(response)
             return JsonResponse({"status": 200})
         except Exception as err:
             print(f"Unexpected {err=}, {type(err)=}")
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse(
+                {"status": 401, "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
 
