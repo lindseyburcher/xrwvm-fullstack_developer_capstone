5de0a5686a06af609ae2c77f17d57953756ed215
lindseyburcher <lindseyburcher@gmail.com>
2024-03-15 16:43:32 -0400

    linter code

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -85,11 +85,6 @@ def registration(request):
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-# Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
-
 
 def get_dealerships(request, state="All"):
     if (state == "All"):
@@ -128,7 +123,7 @@ def get_dealer_details(request, dealer_id):
 
 # Create a `add_review` view to submit a review
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
             response = post_review(data)
