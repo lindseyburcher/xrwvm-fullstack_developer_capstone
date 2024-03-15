6008c1af5183b97eeec31e5b23300b8be0c2c01a
lindseyburcher <135819189+lindseyburcher@users.noreply.github.com>
2024-03-15 09:51:47 +0000

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -12,11 +12,12 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
-    def __str__(self): 
+    def __str__(self):
         return self.name
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -28,6 +29,7 @@ class CarMake(models.Model):
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
@@ -37,7 +39,7 @@ class CarModel(models.Model):
         ('WAGON', 'Wagon'),
         ('HATCHBACK', 'Hatchback'),
         ('CROSSOVER', 'Crossover'),
-        ('MPV', 'MPV'),
+        ('MPV', 'MPV')
     ]
     type = models.CharField(max_length=10,choices=CAR_TYPES, default='HATCHBACK')
     year = models.IntegerField(default=2024, 
