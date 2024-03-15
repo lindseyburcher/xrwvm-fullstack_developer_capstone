a462a558f3632366bc62ce09d3f875337fcba2e3
lindseyburcher <135819189+lindseyburcher@users.noreply.github.com>
2024-03-15 10:20:05 +0000

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,5 +1,6 @@
 from .models import CarMake, CarModel
 
+
 def initiate():
     car_make_data = [
         {"name": "NISSAN", "description": "Great cars. Japanese technology"},
@@ -22,8 +23,8 @@ def initiate():
       {"name": "C-Class", "type": "SUV", "year": 2023, "car_make": car_make_instances[1]},
       {"name": "E-Class", "type": "SUV", "year": 2023, "car_make": car_make_instances[1]},
       {"name": "A4", "type": "SUV", "year": 2023, "car_make": car_make_instances[2]},
-      {"name": "A5", "type":"SUV", "year": 2023, "car_make": car_make_instances[2]},
-      {"name": "A6", "type":"SUV", "year": 2023, "car_make": car_make_instances[2]},
+      {"name": "A5", "type": "SUV", "year": 2023, "car_make": car_make_instances[2]},
+      {"name": "A6", "type": "SUV", "year": 2023, "car_make": car_make_instances[2]},
       {"name": "Sorrento", "type": "SUV", "year": 2023, "car_make": car_make_instances[3]},
       {"name": "Carnival", "type": "SUV", "year": 2023, "car_make": car_make_instances[3]},
       {"name": "Cerato", "type": "Sedan", "year": 2023, "car_make": car_make_instances[3]},
@@ -33,6 +34,5 @@ def initiate():
         # Add more CarModel instances as needed
                     ]
 
-    for data in car_model_data:
-            CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
-        
+for data in car_model_data:
+    CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
