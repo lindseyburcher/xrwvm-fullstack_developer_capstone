36633194e471fe1c0b50b81e9bfdd5750e0e1075
lindseyburcher <135819189+lindseyburcher@users.noreply.github.com>
2024-03-15 09:59:08 +0000

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -15,7 +15,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+
 class CarMake(models.Model):
+    
     name = models.CharField(max_length=100)
     description = models.TextField()
 
@@ -32,22 +34,23 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
+
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
-        ('SEDAN', 'Sedan'), 
-        ('SUV', 'SUV'), 
-        ('WAGON', 'Wagon'), 
-        ('HATCHBACK', 'Hatchback'), 
-        ('CROSSOVER', 'Crossover'), 
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('HATCHBACK', 'Hatchback'),
+        ('CROSSOVER', 'Crossover'),
         ('MPV', 'MPV')
     ]
     type = models.CharField(max_length=10,choices=CAR_TYPES, default='HATCHBACK')
     year = models.IntegerField(default=2024, 
                                validators=[MaxValueValidator(2024), 
                                            MinValueValidator(2015)
-        ])
+                                          ])
 
     def __str__(self):
         return self.name
